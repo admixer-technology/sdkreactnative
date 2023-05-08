1dbeffc703356d4c53883be8c4df248149070f95
Ivan Hanzha <gvanjustfun@gmail.com>
2023-05-08 15:18:30 +0300

    update version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -76,64 +76,64 @@ const App = () => {
 
   // Banner in list
 
-  // const [data, setData] = React.useState([
-  //   { id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }, { id: 10 },
-  //   { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }, { id: 17 }, { id: 18 }, { id: 19 }, { id: 20 }
-  // ]);
+  const [data, setData] = React.useState([
+    { id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }, { id: 10 },
+    { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }, { id: 17 }, { id: 18 }, { id: 19 }, { id: 20 }
+  ]);
 
-  // const addItemsPress = () => {
-  //   const size = data.length;
-  //   const newData = [];
-  //   for (let i = size; i < size + 10; i++) {
-  //     newData.push({ id: i });
-  //   }
-  //   setData(state => [...state, ...newData]);
-  // }
+  const addItemsPress = () => {
+    const size = data.length;
+    const newData = [];
+    for (let i = size; i < size + 10; i++) {
+      newData.push({ id: i });
+    }
+    setData(state => [...state, ...newData]);
+  }
 
-  // const Item = ({ id }) => (
-  //   <View>
-  //     <Text>{`Item ${id}`}</Text>
-  //   </View>
-  // );
+  const Item = ({ id }) => (
+    <View>
+      <Text>{`Item ${id}`}</Text>
+    </View>
+  );
 
-  // const renderItem = ({ item, index }) => {
-  //   if (index % 5 === 0) {
-  //     return (<View style={{ width: '100%', height: 300}}>
-  //       <AdmixerBanner
-  //         config={{
-  //           zoneId: "f9a26255-08a2-40ec-9667-3ab35e69625a",
-  //           bannerWidth: 300,
-  //           bannerHeight: 250,
-  //           sizes: [[300, 250]],
-  //           loadMode: "when_visible",
-  //         }}
-  //         onAdLoaded={onAdLoaded}
-  //         onAdLoadFailed={onAdLoadFailed}
-  //         onAdClicked={onAdClicked} />
-  //     </View>)
-  //   } else {
-  //     return (<View style={{ width: '100%', height: 300 }}>
-  //       <Item id={item.id} />
-  //     </View>);
-  //   }
-  // }
+  const renderItem = ({ item, index }) => {
+    if (index % 5 === 0) {
+      return (<View style={{ width: '100%', height: 300}}>
+        <AdmixerBanner
+          config={{
+            zoneId: "f9a26255-08a2-40ec-9667-3ab35e69625a",
+            bannerWidth: 300,
+            bannerHeight: 250,
+            sizes: [[300, 250]],
+            loadMode: "when_visible",
+          }}
+          onAdLoaded={onAdLoaded}
+          onAdLoadFailed={onAdLoadFailed}
+          onAdClicked={onAdClicked} />
+      </View>)
+    } else {
+      return (<View style={{ width: '100%', height: 300 }}>
+        <Item id={item.id} />
+      </View>);
+    }
+  }
 
 
-  // return (
-  //   <SafeAreaView >
-  //     <View>
-  //       <Button
-  //         style={{ backgroundColor: 'red' }}
-  //         onPress={addItemsPress}
-  //         title='Add items' />
-  //       <FlatList
-  //         data={data}
-  //         keyExtractor={item => item.id.toString()}
-  //         renderItem={renderItem}
-  //       />
-  //     </View>
-  //   </SafeAreaView>
-  // );
+  return (
+    <SafeAreaView >
+      <View>
+        <Button
+          style={{ backgroundColor: 'red' }}
+          onPress={addItemsPress}
+          title='Add items' />
+        <FlatList
+          data={data}
+          keyExtractor={item => item.id.toString()}
+          renderItem={renderItem}
+        />
+      </View>
+    </SafeAreaView>
+  );
 
   // Single banner
 
@@ -148,29 +148,29 @@ const App = () => {
   //   console.log("Interstitial onAdLoadFailed");
   // });
 
-  return (
-    <SafeAreaView >
-    <StatusBar  />
-    <ScrollView>
-      <View>
-        <Button
-          onPress={showInterstitial}
-          title="Show interstitial"
-        />
-        <AdmixerBanner
-          config={{
-            zoneId: "f9a26255-08a2-40ec-9667-3ab35e69625a",
-            bannerWidth: 300,
-            bannerHeight: 250,
-            sizes:[[300, 250]],
-          }}
-          onAdLoaded={onAdLoaded}
-          onAdLoadFailed={onAdLoadFailed}/>
+  // return (
+  //   <SafeAreaView >
+  //   <StatusBar  />
+  //   <ScrollView>
+  //     <View>
+  //       <Button
+  //         onPress={showInterstitial}
+  //         title="Show interstitial"
+  //       />
+  //       <AdmixerBanner
+  //         config={{
+  //           zoneId: "f9a26255-08a2-40ec-9667-3ab35e69625a",
+  //           bannerWidth: 300,
+  //           bannerHeight: 250,
+  //           sizes:[[300, 250]],
+  //         }}
+  //         onAdLoaded={onAdLoaded}
+  //         onAdLoadFailed={onAdLoadFailed}/>
 
-      </View>
-    </ScrollView>
-  </SafeAreaView>
-  );
+  //     </View>
+  //   </ScrollView>
+  // </SafeAreaView>
+  // );
 
   //Native
 
